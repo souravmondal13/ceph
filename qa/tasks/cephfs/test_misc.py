5f644f4824c697b4d84ef5c276152ee2c5ebc738
Michal Jarzabek <stiopa@gmail.com>
2017-01-15 15:32:04 +0000

    mds/Server.cc: Don't evict a slow client if...
    ... it's the only client
    
    Fixes: http://tracker.ceph.com/issues/17855
    Signed-off-by: Michal Jarzabek <stiopa@gmail.com>

--- qa/tasks/cephfs/test_misc.py
+++ qa/tasks/cephfs/test_misc.py
@@ -2,9 +2,14 @@
 from unittest import SkipTest
 from tasks.cephfs.fuse_mount import FuseMount
 from tasks.cephfs.cephfs_test_case import CephFSTestCase
+import time
 
 class TestMisc(CephFSTestCase):
     CLIENTS_REQUIRED = 2
+
+    LOAD_SETTINGS = ["mds_session_autoclose"]
+    mds_session_autoclose = None
+
     def test_getattr_caps(self):
         """
         Check if MDS recognizes the 'mask' parameter of open request.
@@ -31,3 +36,27 @@ class TestMisc(CephFSTestCase):
         self.mount_b.run_shell(["cat", "testfile"])
 
         self.mount_a.kill_background(p)
+
+    def test_evict_client(self):
+        """
+        Check that a slow client session won't get evicted if it's the
+        only session
+        """
+
+        self.mount_b.umount_wait();
+        ls_data = self._session_list()
+        self.assert_session_count(1, ls_data)
+
+        time.sleep(self.mds_session_autoclose * 1.5)
+        ls_data = self._session_list()
+        self.assert_session_count(1, ls_data)
+
+        self.mount_b.mount()
+        self.mount_b.wait_until_mounted()
+
+        ls_data = self._session_list()
+        self.assert_session_count(2, ls_data)
+
+        time.sleep(self.mds_session_autoclose * 1.5)
+        ls_data = self._session_list()
+        self.assert_session_count(1, ls_data)
